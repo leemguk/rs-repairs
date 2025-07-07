2f3d652c4e060c37ce768c30fe5e488a1c818c85
Lee Gilbert <lee.gilbert@ransomspares.co.uk>
2025-07-07 16:33:29 +0100

    Booking Form widget

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,23 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  async headers() {
+    return [
+      {
+        source: '/widget/booking',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'ALLOWALL',
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: 'frame-ancestors *',
+          },
+        ],
+      },
+    ]
+  },
 }
 
 export default nextConfig
