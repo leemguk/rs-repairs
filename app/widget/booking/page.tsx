c97fbbc58072d6b7c813b9f35f33ba4be70ea1d2
Lee Gilbert <lee.gilbert@ransomspares.co.uk>
2025-07-07 17:03:07 +0100

    Test Widget

--- app/widget/booking/page.tsx
+++ app/widget/booking/page.tsx
@@ -1,10 +1,3 @@
-//
-import { BookingForm } from '@/components/booking-form'
-
 export default function BookingWidgetPage() {
-  return (
-    <main className="min-h-screen bg-gray-50 flex items-center justify-center">
-      <BookingForm />
-    </main>
-  )
-} 
+    return <div>Widget Test Page</div>
+  }
